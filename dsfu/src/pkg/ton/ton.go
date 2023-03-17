c4d082f56fc4bbfb14e5ca1980ac200b42041ea2
Vadim Filimonov <fffilimonov@yandex.ru>
2023-03-17 15:21:47 +0200

    fix wallet version

--- dsfu/src/pkg/ton/ton.go
+++ dsfu/src/pkg/ton/ton.go
@@ -9,7 +9,7 @@ import (
 
 func GetClientPubKey(userWalletAddr string) (ed25519.PublicKey, error) {
 	var result ed25519.PublicKey
-	nodeToncli, err := NewNodeToncli(os.Getenv("TON_SEED"), wallet.V4R2, os.Getenv("TON_MASTER_CONTRACT"))
+	nodeToncli, err := NewNodeToncli(os.Getenv("TON_SEED"), wallet.V3R2, os.Getenv("TON_MASTER_CONTRACT"))
 	if err != nil {
 		log.Printf("NewNodeToncli %v", os.Getenv("TON_MASTER_CONTRACT"))
 		return result, err
@@ -24,7 +24,7 @@ func GetClientPubKey(userWalletAddr string) (ed25519.PublicKey, error) {
 }
 
 func CreateCall(userWalletAddr string, userSign []byte, userMsg []byte) error {
-	nodeToncli, err := NewNodeToncli(os.Getenv("TON_SEED"), wallet.V4R2, os.Getenv("TON_MASTER_CONTRACT"))
+	nodeToncli, err := NewNodeToncli(os.Getenv("TON_SEED"), wallet.V3R2, os.Getenv("TON_MASTER_CONTRACT"))
 	if err != nil {
 		return err
 	}
@@ -32,7 +32,7 @@ func CreateCall(userWalletAddr string, userSign []byte, userMsg []byte) error {
 }
 
 func GetSignature(data []byte) ([]byte, error) {
-	nodeToncli, err := NewNodeToncli(os.Getenv("TON_SEED"), wallet.V4R2, os.Getenv("TON_MASTER_CONTRACT"))
+	nodeToncli, err := NewNodeToncli(os.Getenv("TON_SEED"), wallet.V3R2, os.Getenv("TON_MASTER_CONTRACT"))
 	if err != nil {
 		return nil, err
 	}
@@ -41,7 +41,7 @@ func GetSignature(data []byte) ([]byte, error) {
 }
 
 func EndCall(userWalletAddr string, userSign []byte, userMsg []byte) error {
-	nodeToncli, err := NewNodeToncli(os.Getenv("TON_SEED"), wallet.V4R2, os.Getenv("TON_MASTER_CONTRACT"))
+	nodeToncli, err := NewNodeToncli(os.Getenv("TON_SEED"), wallet.V3R2, os.Getenv("TON_MASTER_CONTRACT"))
 	if err != nil {
 		return err
 	}
